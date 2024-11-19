9377f0a1d4628b49a0237c6a880c05caeaf457aa
Vidur Khanal <vidurkhanal@gmail.com>
2024-11-18 23:03:25 -0500

    feat: fetcher for all the debuginfos/elf

---
+++ src/debuginfo_store/fetcher.rs
@@ -0,0 +1,53 @@
+use super::DebugInfod;
+use crate::debuginfopb::{debuginfo::Source, Debuginfo};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+#[derive(Debug, Default)]
+pub struct DebuginfoFetcher {
+    bucket: Arc<Mutex<HashMap<String, Arc<Vec<u8>>>>>,
+    debuginfod: Arc<Mutex<DebugInfod>>,
+}
+
+impl DebuginfoFetcher {
+    pub fn new(
+        bucket: Arc<Mutex<HashMap<String, Arc<Vec<u8>>>>>,
+        debuginfod: Arc<Mutex<DebugInfod>>,
+    ) -> Self {
+        Self { bucket, debuginfod }
+    }
+
+    pub fn fetch_debug_info(&self, dbginfo: &Debuginfo) -> Result<Arc<Vec<u8>>, tonic::Status> {
+        let source = dbginfo.source();
+        match source {
+            Source::Debuginfod => self.fetch_debuginfod(dbginfo),
+            Source::Upload => self.fetch_bucket(dbginfo),
+            _ => Err(tonic::Status::internal("Unknown source in Debuginfo")),
+        }
+    }
+
+    fn fetch_debuginfod(&self, dbginfo: &Debuginfo) -> Result<Arc<Vec<u8>>, tonic::Status> {
+        let mut debuginfod = match self.debuginfod.lock() {
+            Ok(debuginfod) => debuginfod,
+            Err(_) => return Err(tonic::Status::internal("Failed to lock DebugInfod")),
+        };
+
+        let servers = debuginfod.upstream_servers.clone();
+        let rc = debuginfod.get(&servers[0], dbginfo.build_id.as_str())?;
+        return Ok(Arc::clone(&rc));
+    }
+
+    fn fetch_bucket(&self, dbginfo: &Debuginfo) -> Result<Arc<Vec<u8>>, tonic::Status> {
+        let bucket = match self.bucket.lock() {
+            Ok(bucket) => bucket,
+            Err(_) => return Err(tonic::Status::internal("Failed to lock bucket")),
+        };
+        let path = &dbginfo.upload.as_ref().unwrap().id;
+        if let Some(rc) = bucket.get(path) {
+            return Ok(Arc::clone(rc));
+        }
+        Err(tonic::Status::internal("No data found in bucket"))
+    }
+}
